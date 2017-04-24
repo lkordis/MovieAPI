a0668c9b18a40f2781a21c626d7dfa799c89fbd4
Lovro Kordiš <kordis.lovro@gmail.com>
2017-04-24 22:21:37 +0200

    Replaced eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Proba
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
