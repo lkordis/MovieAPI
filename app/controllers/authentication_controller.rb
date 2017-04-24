4d28982b8932bcd95658697585fb38e916c27d11
Lovro Kordiš <kordis.lovro@gmail.com>
2017-04-24 22:33:46 +0200

    Debugging

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,6 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :authenticate_request 
+    skip_before_action :authenticate_request
+    skip_before_action :verify_authenticity_token
     
     def authenticate 
         command = AuthenticateUser.call(params[:email], params[:password]) 
@@ -7,7 +8,7 @@ class AuthenticationController < ApplicationController
         if command.success? 
             render json: { auth_token: command.result, user: current_user } 
         else 
-            render json: { error: command.errors, tu_si: "haha" }, status: :unauthorized 
+            render json: { error: command.errors }, status: :unauthorized 
         end 
     end
 end
