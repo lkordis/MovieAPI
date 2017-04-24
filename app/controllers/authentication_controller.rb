aa497d25a459da7c8a494c5b6508e7d75de699e6
Lovro Kordiš <kordis.lovro@gmail.com>
2017-04-24 22:29:47 +0200

    Debugging

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,7 +7,7 @@ class AuthenticationController < ApplicationController
         if command.success? 
             render json: { auth_token: command.result, user: current_user } 
         else 
-            render json: { error: command.errors }, status: :unauthorized 
+            render json: { error: command.errors, tu_si: "haha" }, status: :unauthorized 
         end 
     end
 end
