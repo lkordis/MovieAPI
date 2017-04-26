9092f974f41c47d5c4fdd52a7adcb2ee6c6518bb
Lovro Kordiš <kordis.lovro@gmail.com>
2017-04-26 15:38:46 +0200

    Minor refactor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,15 +2,17 @@ class MoviesController < ApplicationController
     skip_before_filter :verify_authenticity_token
 
     def user_movies
-        if logged_in?
-            @user = current_user
-            @seen_movies = SeenMovie.where(user_id: @user.id)
-            @wished_movies = WishedMovie.where(user_id: @user.id)
-            @movies = @seen_movies | @wished_movies
+        respond_to do |format|
+            if logged_in?
+                @user = current_user
+                @seen_movies = SeenMovie.where(user_id: @user.id)
+                @wished_movies = WishedMovie.where(user_id: @user.id)
+                @movies = @seen_movies | @wished_movies
 
-            render json: @movies
-        else
-            render json: nil, status: :unprocessable_entity
+                format.json {render json: @movies}
+            else
+                format.json { render json: nil, status: :unprocessable_entity }
+            end
         end
     end
 end
