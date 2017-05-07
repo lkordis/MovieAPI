ddd26e0fa91486d0dccf8522dd71c232fbc795a4
Lovro Kordiš <kordis.lovro@gmail.com>
2017-05-07 18:35:54 +0200

    Added Reviews, deleted uneeded views

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :created_at, :updated_at, :name, :email, :lastName, :password_digest
-json.url user_url(user, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :reviews
 
-  get    '/login',   to: 'sessions#new'
+  get '/login',   to: 'sessions#new'
 
   get '/seen_movies', to: 'seen_movies#index'
   post '/seen_movies', to: 'seen_movies#create'
