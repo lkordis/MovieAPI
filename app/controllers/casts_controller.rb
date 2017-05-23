3c846f502dc1830935eb4eebc3ca7cb4c5523496
Lovro Kordiš <kordis.lovro@gmail.com>
2017-05-23 21:44:54 +0200

    Json builder on people search

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -4,44 +4,55 @@ class CastsController < ApplicationController
     def search
         respond_to do |format|
             if logged_in?
+                @response = Array.new
                 @user = current_user
                 @prefix = params[:query]
                 @names = Cast.where("lower(name) like ?","#{@prefix.downcase}%")
                 @lastNames = Cast.where("lower(lastName) like ?","#{@prefix.downcase}%")
 
+                @seen_movies = SeenMovie.where(user_id: @user.id)
+                @wished_movies = WishedMovie.where(user_id: @user.id)
+
                 @casts = @names | @lastNames
                 @movies = Set.new
+
                 @casts.each do |cast|
+                    @hash = Hash.new
+                    @hash[:id] = cast.id
+                    @hash[:name] = cast.name
+                    @hash[:lastName] = cast.lastName
+
+                    @user_movies = Set.new
+
                     @credits = Credit.where(cast_id: cast.id)
                     @credits.each do |credit|
                         @movie = Movie.find(credit.movie_id)
                         @movies << @movie
                     end
-                end
 
-                @seen_movies = SeenMovie.where(user_id: @user.id)
-                @wished_movies = WishedMovie.where(user_id: @user.id)
-                @user_movies = Set.new
-
-                if !@seen_movies.empty?
-                    @seen_movies.each do |seen_movie|
-                        @movie = Movie.find(seen_movie.movie_id)
-                        if @movies.include? @movie
-                            @user_movies << @movie
+                    if !@seen_movies.empty?
+                        @seen_movies.each do |seen_movie|
+                            @movie = Movie.find(seen_movie.movie_id)
+                            if @movies.include? @movie
+                                @user_movies << @movie
+                            end
                         end
                     end
-                end
 
-                if !@wished_movies.empty?
-                    @wished_movies.each do |wished_movie|
-                        @movie = Movie.find(wished_movie.movie_id)
-                        if @movies.include? @movie
-                            @user_movies << @movie
+                    if !@wished_movies.empty?
+                        @wished_movies.each do |wished_movie|
+                            @movie = Movie.find(wished_movie.movie_id)
+                            if @movies.include? @movie
+                                @user_movies << @movie
+                            end
                         end
                     end
+
+                    @hash[:known_for] = @user_movies
+                    @response << @hash
                 end
 
-                format.json { render json: @user_movies }
+                format.json { render json: @response }
             else
                 format.json { render json: nil, status: :unprocessable_entity }
             end
