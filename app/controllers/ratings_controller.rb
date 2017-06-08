94fd0084cc8ae57e334f55266bb0736ac36a2795
Lovro Kordiš <kordis.lovro@gmail.com>
2017-06-08 09:58:32 +0200

    Ratings routes refactor

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,4 @@
 class RatingsController < ApplicationController
-  before_action :set_rating, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_request
 
   # GET /ratings
@@ -103,15 +102,4 @@ class RatingsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_rating
-      @rating = Rating.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def rating_params
-      params.fetch(:rating, {})
-    end
 end
